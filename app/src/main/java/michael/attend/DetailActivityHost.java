dadbc1737f2581c47acb48e1d363df90c0582ebd
Colin Porter <coln.porter@gmail.com>
2018-06-06 23:05:14 -0700

    GPS Locator update

--- app/src/main/java/michael/attend/DetailActivityHost.java
+++ app/src/main/java/michael/attend/DetailActivityHost.java
@@ -1,18 +1,26 @@
 package michael.attend;
 
+import android.Manifest;
 import android.content.Intent;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DetailActivityHost extends AppCompatActivity {
 
+    String uid;
     ListView listView;
-
+    Button takeAttendance;
     String[] names = {
             "Hung Lo", "LetsGo",
             "TestGuy", "NewGuy"
@@ -27,11 +35,43 @@ public class DetailActivityHost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_host);
+        uid = LoginHome.user_uid;
 
         Intent i = getIntent();
         String title = i.getStringExtra("title");
         String description = i.getStringExtra("description");
         String host = i.getStringExtra("host");
+        final int position = i.getIntExtra("position",0);
+
+        takeAttendance = findViewById(R.id.take_attendance);
+
+        // this records the host's coordinates and sets takeAttendance  to valid
+        takeAttendance.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int request_permission = 1;
+                GPSLocator x = new GPSLocator(getApplicationContext());
+                Location l = x.getLocation();
+                if (l == null) {
+                    ActivityCompat.requestPermissions(DetailActivityHost.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, request_permission);
+
+
+                }
+                if (l != null) {
+                    double lat = l.getLatitude();
+                    double lon = l.getLongitude();
+
+                    //makes attendance valid and record host's coordinates in firebase
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(uid).child("user_groups").child(String.valueOf(position)).child("location").child("lat");
+                    databaseReference.setValue(lat);
+
+                    DatabaseReference databaseReference1 = FirebaseDatabase.getInstance().getReference().child("users").child(uid).child("user_groups").child(String.valueOf(position)).child("location").child("lon");
+                    databaseReference1.setValue(lon);
+
+                }
+            }
+        });
+
+
 
         TextView event_title = (TextView) findViewById(R.id.group_name);
         TextView event_description = (TextView) findViewById(R.id.group_description);
